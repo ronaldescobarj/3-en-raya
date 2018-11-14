940534083a0bf686e3e12540336332417354329d
Ronald Escobar <ronaldescobarj@gmail.com>
2018-11-14 11:46:00 -0400

    test board

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,14 +1,16 @@
 class Board
     rows = 3
     cols = 3
-    board[3][3]
+    # board = Array.new(rows){Array.new(cols)}
+    board = []
 
     def init()
-        for i to: i..3
-            for j to j..3
-                board[i][j] = ' '
-            end
-        end
+        # for i in 0..2
+        #     for j in 0..2
+        #         board[](i,j) = ' '
+        #     end
+        # end
+        board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
     end
 
     def mark_cell(x,y,mark)
--- spec/Board_spec.rb
+++ spec/Board_spec.rb
@@ -1,1 +1,14 @@
-describe
+require './lib/Board'
+
+describe Board do
+
+    before(:each) do
+        @board = Board.new
+    end
+
+    it "deberia inicializar el tablero con espacios" do
+        
+        test_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        expect(@board.init()).to eq test_board
+    end
+end
